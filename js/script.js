33d96eec7364ba6948ac7422003d6db5c4158884
Nikolay Vinogradov <was.bic@gmail.com>
2022-07-08 02:05:08 +0300

    Выполнено задание. Урок 16

--- js/script.js
+++ js/script.js
@@ -1,44 +1,19 @@
 'use strict';
 
-const DomElement = function (selector) {
-  this.selector = selector;
-  this.height = '50px';
-  this.width = '150px';
-  this.bg = 'white';
-  this.border = '1px solid red';
-  this.fontSize = '18px';
-};
-
-DomElement.prototype.createElement = function () {
-  if(this.selector[0] == '.'){
-    const elemClass = this.selector.slice(1);
-    const newDiv = document.createElement('div');
-
-    newDiv.className = elemClass;
-    newDiv.textContent = "It's new Div";
-    newDiv.style.cssText = 
-    `background: ${this.bg}; font-size: ${this.fontSize};;
-    height: ${this.height}; width: ${this.width}; border: ${this.border}`; 
-
-    document.body.append(newDiv);
-
-  } else if (this.selector[0] == '#') {
-    const elemId = this.selector.slice(1);
-    const newPar = document.createElement('p');
-
-    newPar.id = elemId;
-    newPar.textContent = "It's new Paragraph";
-    newPar.style.cssText = 
-    `background: ${this.bg}; font-size: ${this.fontSize};;
-    height: ${this.height}; width: ${this.width}; border: ${this.border}`;
-
-    document.body.append(newPar);
-
+class First {
+  hello() {
+    console.log("Привет я метод родителя!");
   }
-};
+}
 
-const boxClass = new DomElement('.class');
-const boxId = new DomElement('#id');
+class Second extends First {
+ hello() {
+  super.hello();
+  console.log('А я наследуемый метод!');
+  
+ }
+}
+const first = new First();
+const second = new Second();
 
-boxClass.createElement();
-boxId.createElement();
+second.hello();
