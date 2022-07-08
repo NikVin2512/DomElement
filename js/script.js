456bccaff416c60dc5cbb4d3e8b40b9a2caa15b9
Nikolay Vinogradov <was.bic@gmail.com>
2022-07-08 21:51:28 +0300

    fix: constructor and decrease code

--- js/script.js
+++ js/script.js
@@ -1,44 +1,46 @@
 'use strict';
 
-const DomElement = function (selector) {
+
+
+const DomElement = function (selector, height, width, bg, border, fontSize) {
   this.selector = selector;
-  this.height = '50px';
-  this.width = '150px';
-  this.bg = 'white';
-  this.border = '1px solid red';
-  this.fontSize = '18px';
-};
+  this.height = height;
+  this.width = width;
+  this.bg = bg;
+  this.border = border;
+  this.fontSize = fontSize;
 
-DomElement.prototype.createElement = function () {
-  if(this.selector[0] == '.'){
-    const elemClass = this.selector.slice(1);
-    const newDiv = document.createElement('div');
-
-    newDiv.className = elemClass;
-    newDiv.textContent = "It's new Div";
-    newDiv.style.cssText = 
-    `background: ${this.bg}; font-size: ${this.fontSize};;
-    height: ${this.height}; width: ${this.width}; border: ${this.border}`; 
-
-    document.body.append(newDiv);
-
-  } else if (this.selector[0] == '#') {
-    const elemId = this.selector.slice(1);
-    const newPar = document.createElement('p');
-
-    newPar.id = elemId;
-    newPar.textContent = "It's new Paragraph";
-    newPar.style.cssText = 
-    `background: ${this.bg}; font-size: ${this.fontSize};;
+  DomElement.prototype.createElement =  () => {
+    const cssText = `background: ${this.bg}; font-size: ${this.fontSize};;
     height: ${this.height}; width: ${this.width}; border: ${this.border}`;
+    const elemFirst = this.selector.slice(1);
+    if(this.selector[0] == '.'){
+      const newDiv = document.createElement('div');
 
-    document.body.append(newPar);
+      newDiv.className = elemFirst;
+      newDiv.textContent = "It's new Div";
+      newDiv.style.cssText = cssText;
 
-  }
+      document.body.append(newDiv);
+
+    } else if (this.selector[0] == '#') {
+      const newPar = document.createElement('p');
+
+      newPar.id = elemFirst;
+      newPar.textContent = "It's new Paragraph";
+      newPar.style.cssText = cssText;
+
+      document.body.append(newPar);
+
+    }
+  };
 };
 
-const boxClass = new DomElement('.class');
-const boxId = new DomElement('#id');
+
+
+
+const boxClass = new DomElement('.class', '100px', '150px', 'green', '1px solid red', '18px');
+const boxId = new DomElement('#id', '100px', '150px', 'green', '1px solid red', '18px');
 
 boxClass.createElement();
 boxId.createElement();
